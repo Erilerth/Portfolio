9b8d2759a5edc1916b5bfdeb0b98089092c3c887
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-06-03 10:23:42 +0200

    feat: Update language to French and add client-side rendering in AboutCheck component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='fr' suppressHydrationWarning>
       <SpeedInsights />
       <body className={inter.className}>
         <ThemeProvider
